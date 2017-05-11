b908e0796653f5cbda030de2cc5077b069dd3ab1
IT Explore <itexplore@investec.co.za>
2017-05-11 07:56:00 +0200

    for cary

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -1,9 +1,13 @@
+import { bubble as Menu } from 'react-burger-menu'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { setScreen } from '../actions'
 import Picker from '../components/Picker'
 import Posts from '../components/Posts'
+
+import './sidebarstyle.css'
+
 
 class SideBar extends Component {
   static propTypes = {
@@ -13,10 +17,28 @@ class SideBar extends Component {
   render() {
     return (
       <div>
-        <button onClick={() => this.props.setScreen(0)}>Pay</button>
-        <button onClick={() => this.props.setScreen(1)}>Analytics</button>
-        <button onClick={() => this.props.setScreen(2)}>Recieve</button>
-      </div>
+        <Menu id="mymenu">
+          <a id="home" onClick={() => this.props.setScreen(0)} className="menu-item" >Pay</a>
+          <a id="about" onClick={() => this.props.setScreen(1)} className="menu-item" >Analytics</a>
+          <a id="contact" onClick={() => this.props.setScreen(2)} className="menu-item" >Recieve</a>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </Menu>
+          <ul id="transitionList">
+          <h2>Payments: List of transitions</h2>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createLogger } from 'redux-logger'
 import reducer from './reducers'
 import App from './containers/App'
 
+
 const middleware = [ thunk ]
 if (process.env.NODE_ENV !== 'production') {
   // middleware.push(createLogger({
